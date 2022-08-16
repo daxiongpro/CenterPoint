da5289e69926d5d4aec91a54f003964d207609c9
daxiongpro <910660298@qq.com>
2022-08-16 09:26:01 +0800

    bugfix: circular import

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import os
 from setuptools import find_packages, setup
+from setuptools import Extension, dist, find_packages, setup
 
+dist.Distribution().fetch_build_eggs(["Cython", "numpy>=1.11.1"])
 
 def readme():
     try:
@@ -22,6 +24,7 @@ if __name__ == "__main__":
         keywords="computer vision, 3d object detection",
         url="https://github.com/daxiongpro/centerpoint",
         packages=find_packages(exclude=("configs", "tools", "demo")),
+        package_data={"det3d.ops": ["*/*.so"]},
         license="Apache License 2.0",
         setup_requires=["pytest-runner", "cython", "numpy"],
         zip_safe=False,
